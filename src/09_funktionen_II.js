713ec9698a3db6ed5460f46b0e40a1127d5993f9
Viktor7874 <viktorromme247@gmail.com>
2024-07-05 08:44:33 +0000

    09/01 add()/ subtract()/ multiply()

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -7,15 +7,33 @@
 1. Dateneingabe + -überprüfung :  
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+4. Ausgabe in Konsole : check!
 */
 
 
+function add(a,b){
+	return a + b;
+}
 
+function substract(a,b){
+	return a - b;
+}
 
+function multiply(a,b){
+	return a * b;
+}
 
+output(division(3,2));
+output(division(3,0));
+function division (a,b){
 
-
+	if (b != 0) {
+		return a / b;
+	} else {
+		return "Teilen durch 0 nicht möglich";
+	}
+	
+}
 
 // module: output | test:
 // output("hello");
