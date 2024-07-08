aab9c12f5c404c40f7c837a048edc9d3985fcf02
Viktor7874 <viktorromme247@gmail.com>
2024-07-08 08:31:55 +0000

    11/03 Theorie Array

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -6,8 +6,16 @@
  * 
  */
 
+/*** 02. Theorie: Array */
 
+let arr;
 
+// arr= new Array(); //Konstruktor
+arr = [2,11,7,8,10]; //Literal
+output(arr);
+output(arr.length); //Anzahl der Elemente
+output(arr[0]); //index 0 (1. Pos. im Array )
+output (arr[arr.length -1]);
 /**
  *   PRAXIS
  * 
@@ -16,15 +24,17 @@
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
- output(getSentence("Ich","bin","Max","Mütze"));
+//  output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4) {
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 + 
                 PUNCT;
 
     return str;
